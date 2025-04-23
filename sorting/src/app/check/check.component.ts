57d1d10acc858f20bb4f24cf6c30f57827543783
rajmohan2001 <rajmohan8303@gmail.com>
2025-04-23 16:40:55 +0530

    check component created

---
+++ sorting/src/app/check/check.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  imports: [],
+  templateUrl: './check.component.html',
+  styleUrl: './check.component.css'
+})
+export class CheckComponent {
+
+}
